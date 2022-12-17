bb59908f784008dc27e393bcd4f74821113aebff
LuQuintas11 <luciaquintas14@gmail.com>
2022-12-17 05:09:24 +0000

    set up allauth

--- surfparadise/settings.py
+++ surfparadise/settings.py
@@ -105,9 +105,10 @@ WSGI_APPLICATION = 'surfparadise.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
