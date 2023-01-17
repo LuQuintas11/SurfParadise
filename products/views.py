e6d668ace8c8141a174f5cd1416504a7e74f2599
LuQuintas11 <luciaquintas14@gmail.com>
2023-01-17 01:57:14 +0000

    added form for shopping bag

--- products/views.py
+++ products/views.py
@@ -11,6 +11,22 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
+    sort = None
+    direction = None
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
     if request.GET:
         if 'q' in request.GET:
