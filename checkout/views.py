79ff7c7c32f25f21dd0fa6ff31afbd5c157da389
LuQuintas11 <luciaquintas14@gmail.com>
2023-01-18 11:20:03 +0000

    Added strip payment funcionanility

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key':'pk_test_51MRSwXEPIeyrbiZgo64ge5b8eUXPFqrKNtEqBCBVun3wtVxxyqYlHgxI4gkGNmVR1bqJ0rfQ53ATVj6cNMsy1uCL00OIE6nHmr',
+        'client_secret': 'test_client_secret',
     }
 
     return render(request, template, context)
