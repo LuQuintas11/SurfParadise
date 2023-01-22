546cf7be9471ef346d280967a4717bdf5f87bd49
LuQuintas11 <luciaquintas14@gmail.com>
2023-01-22 12:00:19 +0000

    Added funcionality to contact form

--- products/models.py
+++ products/models.py
@@ -34,7 +34,7 @@ class Product(models.Model):
     favourites = models.ManyToManyField(
         User, related_name='favourite', default=None, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+  
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
--- products/views.py
+++ products/views.py
@@ -16,23 +16,13 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
-    sort = None
-    direction = None
-    if request.GET:
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
-            sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
-
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
-            products = products.order_by(sortkey)
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -42,13 +32,10 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    category = request.GET.get("category", None)
-    if category:
-        products = products.filter(category__name=category) 
-
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
