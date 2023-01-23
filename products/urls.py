8e9130d63ec16b50fe03df5db7a95760d5bb9b56
LuQuintas11 <luciaquintas14@gmail.com>
2023-01-23 01:26:47 +0000

    product admin-securing the views

--- products/urls.py
+++ products/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('<int:product_id>', views.product_detail, name='product_detail'),
     path('fav/<int:id>/', views.favourite_add, name='favourite_add'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
    
 ]
 if settings.DEBUG:
