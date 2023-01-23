0526478617adc0a125df2c50395a7d352b172fe0
LuQuintas11 <luciaquintas14@gmail.com>
2023-01-23 10:53:51 +0000

    deployment part 5

--- surfparadise/settings.py
+++ surfparadise/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'checkout',
     'crispy_forms',
     'contact',
+    'storages',
     
 ]
 MIDDLEWARE = [
@@ -191,7 +192,22 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+if 'USE_AWS' in os.environ:
 
+    AWS_STORAGE_BUCKET_NAME = 'surf-paradise'
+    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
 
