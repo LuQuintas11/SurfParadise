cf57769649186ee588cf2befd3655d48f8be11f4
LuQuintas11 <luciaquintas14@gmail.com>
2023-01-23 08:23:08 +0000

    deplyment part 2

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -28,7 +28,7 @@ class StripeWH_Handler:
         pid = intent.id
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
-        # Get the Charge object
+     
         stripe_charge = stripe.Charge.retrieve(
         intent.latest_charge
         )
@@ -37,7 +37,7 @@ class StripeWH_Handler:
         shipping_details = intent.shipping
         grand_total = round(intent.charges.data[0].amount / 100, 2)
 
-        # Clean data in the shipping details
+     
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
