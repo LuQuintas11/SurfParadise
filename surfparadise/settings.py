1d27a1820f4bc5af352cc654c24974704ecc44b2
LuQuintas11 <luciaquintas14@gmail.com>
2023-01-23 08:48:40 +0000

    remove secret key and set debug

--- surfparadise/settings.py
+++ surfparadise/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l55oq^%kl$9)vqebnlh6@%dp1_ufl*j$gs3t=z6vel7r#$ay2+'
+SECRET_KEY = os.environ.get('SECRET_KET', '')
+# 'django-insecure-l55oq^%kl$9)vqebnlh6@%dp1_ufl*j$gs3t=z6vel7r#$ay2+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = []
 
