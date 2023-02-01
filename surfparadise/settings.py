f04d8ff408acffe496d06428b71e8a3ba9bd6ff1
LuQuintas11 <luciaquintas14@gmail.com>
2023-02-01 14:30:22 +0000

    set dinamic DEBUG

--- surfparadise/settings.py
+++ surfparadise/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
-DEBUG = False
-#"DEVELOPMENT" in os.environ
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ["surfparadise.herokuapp.com", "localhost"]
 
